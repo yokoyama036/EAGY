97fe6a867322708afe993a8956a05f9db4cc22fd
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-04-21 14:43:32 +0900

    devise エラーメッセージ調整

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,14 @@
-FactoryBot.define do
-  factory :user do
-    email { '123456@gmail.com' }
-    password { "123456" }
-    password_confirmation { "123456" }
-    admin { true }
-  end
-  factory :user2, class: User do
-    email  { '222222@gmail.com' }
-    password { "222222" }
-    password_confirmation { "222222" }
-    admin { false }
-  end
-end
+# FactoryBot.define do
+#   factory :user do
+#     email { '123456@gmail.com' }
+#     password { "123456" }
+#     password_confirmation { "123456" }
+#     admin { true }
+#   end
+#   factory :user2, class: User do
+#     email  { '222222@gmail.com' }
+#     password { "222222" }
+#     password_confirmation { "222222" }
+#     admin { false }
+#   end
+# end
