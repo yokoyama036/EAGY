3876a5c27cc0da0e83db724eb482aec6d38120e1
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-04-22 11:52:59 +0900

    バリデーション変更

--- app/models/user_physical_information.rb
+++ app/models/user_physical_information.rb
@@ -3,11 +3,4 @@ class UserPhysicalInformation < ApplicationRecord
   has_one_attached :image
   validates :metabolism, presence: { message: '必要エネルギー量が計算されていません。' }, numericality: true
 
-  attr_accessor :height, :weight, :age, :activity_level
-
-  # バリデーションを追加
-  validates :height, presence: true
-  validates :weight, presence: true
-  validates :age, presence: true
-  validates :activity_level, presence: true
 end
