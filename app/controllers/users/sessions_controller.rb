292c79724a6b64fc0b228c5aeeab65263f355576
yokoyama <yokoyama@DESKTOP-5P6MFQI.localdomain>
2023-05-17 22:57:32 +0900

    ゲストログイン後の遷移先修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in User.guest  
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to after_sign_in_path_for(user), notice: 'ゲストユーザーとしてログインしました。'
   end
   def guest_admin_sign_in
     user = User.guest_admin
